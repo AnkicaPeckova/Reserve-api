07e4662a7602a975811ae9a82f28a0311a15d248
Andrej Sarafimovski <andrejsarafimovski@gmail.com>
2021-04-24 11:50:32 +0200

    feat: updated some endpoints

--- src/app.py
+++ src/app.py
@@ -5,38 +5,39 @@ from flask_sqlalchemy import SQLAlchemy
 
 connexion_app = connexion.App(__name__, specification_dir="./")
 app = connexion_app.app
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@postgres:5432/reserve-db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///reserve-db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+inventory_ms_base_url = 'http://localhost:5001/api'
+payment_ms_base_url = 'http://localhost:5002/api'
+location_ms_base_url = 'http://localhost:5003/api/locations'
+
 
 import models
 
 http = urllib3.PoolManager()
-
-def healthz():
-    return { 'healthy': True }
 
 def getAllTransferProducts():
     transferProductsRent = db.session.query(models.TransferProductRent).all()
     return transferProductsRent, 200
 
 def rentTransferProduct(product_id):
-    res = http.request('POST',"http://localhost:5008/api/get/all_products_rent")
+    res = http.request('POST',"{inventory_ms_base_url}/get/all_products_rent")
     products = json.loads(res.data.decode('utf-8'))
     exists = False
     for product in products:
         exists = product.id==product_id
     if exists == False:
         return {'message':'Product doesn\'t exist!'},404
-    http.request('POST',"http://localhost:5008/api/reserve_product_rent",fields={
-        product_id,
-        user_id: 'f6b03ce5-d299-4fb1-84d0-76c09abee2a2' # PLACEHOLDER
-    })
+    http.request('GET',"{inventory_ms_base_url}/rent_product/{product_id}/{user_id}")
     return { 'done': True }
 
 def payTransferProductRent():
-    http.request('POST',"http://localhost:5005/api/pay/rent_pay",fields={
-        amount: 5 # PLACEHOLDER
+    res = http.request('GET',"{inventory_ms_base_url}/get_price_for_product_rent/{product_id}")
+    price = res.data.decode('utf-8')
+    http.request('POST',"{payment_ms_base_url}/pay/rent_pay",fields={
+        amount: price
     })
     return {'done':True}
 
@@ -45,8 +46,8 @@ def parkTransferProduct(product_id,parking_spot_id,parking_zone_id):
     return { 'done': True }
 
 def payTransferProductPark(product_id,parking_spot_id,parking_zone_id):
-    http.request('POST',"http://localhost:5005/api/locations/parking_spots/{parking_spot_id}/free/{parking_zone_id}")
-    http.request('POST',"http://localhost:5005/api/pay/parking_pay",fields={
+    http.request('PUT',"{location_ms_base_url}/parking_spots/{parking_spot_id}/reserve/{parking_zone_id}")
+    http.request('POST',"{payment_ms_base_url}/pay/parking_pay",fields={
         amount: 5 # PLACEHOLDER
     })
     return {'done':True}
