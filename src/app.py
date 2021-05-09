a1d82615b342c387d384d4b251f184ba7145fbcf
Andrej Sarafimovski <andrejsarafimovski@gmail.com>
2021-05-09 14:03:33 +0200

    feat: Added consul, added migrations, updated APIs, updated Swagger, added Health Route

--- src/app.py
+++ src/app.py
@@ -1,13 +1,21 @@
+import configparser
+import netifaces
 from functools import wraps
 import connexion
 import urllib3
 from flask_migrate import Migrate
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 import jwt
+from consul import Consul, Check
 import time
 
 JWT_SECRET = '3cZDYwjsbHYwR94w'
 JWT_LIFETIME_SECONDS = 600000
+
+consul_port = 5001
+service_name = "reserve"
+service_port = 5000
 
 connexion_app = connexion.App(__name__, specification_dir="./")
 app = connexion_app.app
@@ -23,6 +31,40 @@ location_ms_base_url = 'http://localhost:5003/api/locations'
 import models
 
 http = urllib3.PoolManager()
+
+def get_ip():
+    interface_name = config_parser['NETWORK']['interface']
+    ip = netifaces.ifaddresses(interface_name)[netifaces.AF_INET][0]["addr"]
+    return ip
+
+def register_to_consul():
+    consul = Consul(host="consul", port=consul_port)
+
+    agent = consul.agent
+
+    service = agent.service
+
+    ip = get_ip()
+
+    check = Check.http(f"http://{ip}:{service_port}/", interval="10s", timeout="5s", deregister="1s")
+
+    service.register(service_name, service_id=service_name, address=ip, port=service_port, check=check)
+
+def get_service(service_id):
+    consul = Consul(host="consul", port=consul_port)
+
+    agent = consul.agent
+
+    service_list = agent.services()
+
+    service_info = service_list[service_id]
+
+    return service_info['Address'], service_info['Port']
+
+register_to_consul()
+
+def index():
+    return "reserve"
 
 def has_role(arg):
     def has_role_inner(fn):
@@ -48,7 +90,8 @@ def has_role(arg):
 @has_role('reserve')
 def getAllTransferProducts():
     transferProductsRent = db.session.query(models.TransferProductRent).all()
-    return transferProductsRent, 200
+    transferProductsPark = db.session.query(models.TransferProductPark).all()
+    return { transferProductsRent, transferProductsPark }, 200
 
 @has_role('reserve')
 def rentTransferProduct(product_id):
@@ -60,32 +103,63 @@ def rentTransferProduct(product_id):
     if exists == False:
         return {'message':'Product doesn\'t exist!'},404
     http.request('GET',"{inventory_ms_base_url}/rent_product/{product_id}/{user_id}")
-    return { 'done': True }
+    transferProductRent = models.TransferProductRent(
+        parkedByUser = extract_user_id(request.headers),
+        dateParked = datetime.now(),
+        product_id = product_id
+    )
+    db.session.add(transferProductRent)
+    db.session.commit()
+    return { transfer_product_rent_id: transferProductRent.id }
 
 @has_role('reserve')
-def payTransferProductRent():
+def payTransferProductRent(transfer_product_rent_id):
+    user_id = extract_user_id(request.headers)
+    transferProductRent = db.session.query(models.TransferProductRent).filter_by(id=transfer_product_rent_id).first()
     res = http.request('GET',"{inventory_ms_base_url}/get_price_for_product_rent/{product_id}")
     price = res.data.decode('utf-8')
     http.request('POST',"{payment_ms_base_url}/pay/rent_pay",fields={
-        amount: price
+        amount: price,
+        user_id:user_id
     })
+    db.session.delete(transferProductRent)
+    db.session.commit()
     return {'done':True}
 
 @has_role('reserve')
 def parkTransferProduct(product_id,parking_spot_id,parking_zone_id):
+    user_id = extract_user_id(request.headers)
     http.request('POST',"http://localhost:5005/api/locations/parking_spots/{parking_spot_id}/reserve/{parking_zone_id}")
-    return { 'done': True }
+    transferProductPark = models.TransferProductPark(
+        parkedByUser = extract_user_id(request.headers),
+        dateParked = datetime.now(),
+        product_id = product_id,
+        parking_spot_id = parking_spot_id,
+        parking_zone_id = parking_zone_id
+    )
+    db.session.add(transferProductPark)
+    db.session.commit()
+    return { transfer_product_park_id: transferProductPark.id }
 
 @has_role('reserve')
-def payTransferProductPark(product_id,parking_spot_id,parking_zone_id):
+def payTransferProductPark(transfer_product_park_id):
+    transferProductPark = db.session.query(models.TransferProductPark).filter_by(id=transfer_product_park_id).first()
     http.request('PUT',"{location_ms_base_url}/parking_spots/{parking_spot_id}/reserve/{parking_zone_id}")
     http.request('POST',"{payment_ms_base_url}/pay/parking_pay",fields={
         amount: 5 # PLACEHOLDER
     })
+    db.session.delete(transferProductPark)
+    db.session.commit()
     return {'done':True}
 
 def decode_token(token):
     return jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+
+def extract_user_id(headers):
+    headers = request.headers
+    token = headers['AUTHORIZATION'].split(' ')[1]
+    decoded_token = decode_token(token)
+    return decode_token.sub
 
 connexion_app.add_api("api.yaml")
 
--- src/models.py
+++ src/models.py
@@ -3,6 +3,15 @@ from app import db
 class TransferProductRent(db.Model):
     __tablename__ = "transfer_product_rent"
     id = db.Column(db.Integer, primary_key=True)
-    locationWhereToRent = db.Column(db.String(16))
-    locationToLeave = db.Column(db.String(16))
-    boolean = db.Column(db.Boolean)
+    rentedByUser = db.Column(db.String(36))
+    dateRented = db.Column(db.DateTime)
+    product_id = db.Column(db.Integer)
+
+class TransferProductPark(db.Model):
+    __tablename__ = "transfer_product_park"
+    id = db.Column(db.Integer, primary_key=True)
+    parkedByUser = db.Column(db.String(36))
+    dateParked = db.Column(db.DateTime)
+    product_id = db.Column(db.Integer)
+    parking_spot_id = db.Column(db.Integer)
+    parking_zone_id = db.Column(db.Integer)
